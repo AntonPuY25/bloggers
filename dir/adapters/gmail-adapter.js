0774e6bb296c3bc42de66d7eda4eb00bc707ce07
Anton Kliaschonak <PuY25@bk.ru>
2022-10-24 18:59:00 +0300

    Resend Email

--- dir/adapters/gmail-adapter.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=gmail-adapter.js.map
--- dir/settings/settings.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.settings = void 0;
-exports.settings = {
-    MONGO_URI: process.env.PORT || 3000,
-    JWT_SECRET: process.env.JWT_SECRET || '123'
-};
-//# sourceMappingURL=settings.js.map

59ea95a8b11f21726de9e10206d5e4905f78eed5
Anton Kliaschonak <PuY25@bk.ru>
2022-11-01 19:41:46 +0300

    Vercel-Deploy

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import cors from "cors";
 import {Request, Response} from "express";
 import {bloggersRoute} from "./Routes/bloggers-route";
-import bodyParser from "body-parser";
 import {postsRoute} from "./Routes/posts-route";
 import {testingRoute} from "./Routes/testing-route";
 import mongoose from "mongoose";
@@ -10,7 +9,10 @@ import {authRoute} from "./Routes/auth-route";
 import {settings} from "./settings/settings";
 import {commentsRoute} from "./Routes/comments-route";
 import {emailRouter} from "./Routes/email-router";
+import * as dotenv from 'dotenv';
 
+
+dotenv.config()
 const express = require('express')
 export const app = express()
 const port = settings.MONGO_URI
@@ -18,14 +20,17 @@ const cookieParser = require("cookie-parser");
 
 app.use(cors())
 app.use(cookieParser());
-// app.use(bodyParser())
 app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
-const mongoUri = 'mongodb+srv://PuY:5718096010@cluster0.kusse.mongodb.net/Lesson?retryWrites=true&w=majority';
+const mongoUri = process.env.MONGO_URI;
+
+if (!mongoUri) {
+    throw new Error('Url isn\'t find');
+}
 
 app.use('/blogs', bloggersRoute)
 app.use('/posts', postsRoute)

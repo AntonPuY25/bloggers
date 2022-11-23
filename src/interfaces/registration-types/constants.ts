8a8169380ad9f6b543d3e62c208a7b3c695af10a
Anton Kliaschonak <PuY25@bk.ru>
2022-11-23 21:47:26 +0300

    Fix bugs by tests

--- src/interfaces/registration-types/constants.ts
+++ src/interfaces/registration-types/constants.ts
@@ -1,4 +1,2 @@
-//TODO: если что - вернуть обратно (20, 30)
-
-export const ACCESS_TOKEN_TIME = '60s';
-export const REFRESH_TOKEN_TIME = '120s';
+export const ACCESS_TOKEN_TIME = '20s';
+export const REFRESH_TOKEN_TIME = '30s';
